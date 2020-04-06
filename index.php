9177518cbf0ead759dac68bcebdad64d734ba38e
oleg216 <58627165+oleg216@users.noreply.github.com>
2020-04-06 22:09:54 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,206 @@
+<?php
+require_once 'kclick_client.php';
+$client = new KClickClient('http://z68960dw.beget.tech/api.php', 'qmp5gvg7vbfss4rktqg9ckcb3219jkgm');
+$client->sendAllParams();       // to send all params from page query
+$client->params('');  // send params like param1=c&param2=b
+// $client->sendUtmLabels();     // to send only utm labels
+// $client->debug();              // to enable debug mode and show the errors
+// $client->currentPageAsReferrer(); // to send current page as referrer value
+// $client->keyword('KEYWORD');  // send custom keyword
+// $client->param('extra_param_5', '123'); // you can send any params
+$client->execute();             // request to api, show the output and continue
+// $client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
+
+<!DOCTYPE html>
+<html lang="ru-RU"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>«Один день из жизни человека, однажды разочаровавшегося в сыре». | monoporn</title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="http://monoporn.ru/xmlrpc.php">
+<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+<link rel="dns-prefetch" href="http://s.w.org/">
+<link rel="alternate" type="application/rss+xml" title="monoporn » Лента" href="http://monoporn.ru/feed/">
+<link rel="alternate" type="application/rss+xml" title="monoporn » Лента комментариев" href="http://monoporn.ru/comments/feed/">
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/monoporn.ru\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.8.12"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,56826,8203,55356,56819),0,0),c=j.toDataURL(),b!==c&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55358,56794,8205,9794,65039),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55358,56794,8203,9794,65039),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script><script src="./index_files/wp-emoji-release.min.js.Без названия" type="text/javascript" defer=""></script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel="stylesheet" id="easy-wp-page-nav-css" href="./index_files/easy-wp-pagenavigation.css" type="text/css" media="all">
+<link rel="stylesheet" id="graphy-fonts-css" href="./index_files/css" type="text/css" media="all">
+<link rel="stylesheet" id="graphy-genericons-css" href="./index_files/genericons.css" type="text/css" media="all">
+<link rel="stylesheet" id="graphy-style-css" href="./index_files/style.css" type="text/css" media="all">
+<script type="text/javascript" src="./index_files/jquery.js.Без названия"></script>
+<script type="text/javascript" src="./index_files/jquery-migrate.min.js.Без названия"></script>
+<link rel="https://api.w.org/" href="http://monoporn.ru/wp-json/">
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://monoporn.ru/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://monoporn.ru/wp-includes/wlwmanifest.xml"> 
+<link rel="prev" title="«Маковые поля наших возможностей»" href="http://monoporn.ru/2017/09/21/%d0%bf%d0%be-%d0%bc%d0%bd%d0%b5-%d1%82%d0%b0%d0%ba-%d0%b2%d1%81%d0%b5%d0%b3%d0%be-%d0%bb%d0%b8%d1%88%d1%8c-%d1%80%d0%b0%d1%81%d1%81%d0%ba%d0%b0%d0%b7-%d0%bd%d0%be-%d0%b4%d0%bb%d1%8f-%d1%82%d0%b5/">
+<link rel="next" title="«из потерянного..»" href="http://monoporn.ru/2017/10/25/%d0%b8%d0%b7-%d0%bf%d0%be%d1%82%d0%b5%d1%80%d1%8f%d0%bd%d0%bd%d0%be%d0%b3%d0%be/">
+<meta name="generator" content="WordPress 4.8.12">
+<link rel="canonical" href="http://monoporn.ru/2017/10/17/%d0%be%d0%b4%d0%b8%d0%bd-%d0%b4%d0%b5%d0%bd%d1%8c-%d0%b8%d0%b7-%d0%b6%d0%b8%d0%b7%d0%bd%d0%b8-%d1%87%d0%b5%d0%bb%d0%be%d0%b2%d0%b5%d0%ba%d0%b0-%d0%be%d0%b4%d0%bd%d0%b0%d0%b6%d0%b4%d1%8b-%d1%80/">
+<link rel="shortlink" href="http://monoporn.ru/?p=402">
+<link rel="alternate" type="application/json+oembed" href="http://monoporn.ru/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fmonoporn.ru%2F2017%2F10%2F17%2F%25d0%25be%25d0%25b4%25d0%25b8%25d0%25bd-%25d0%25b4%25d0%25b5%25d0%25bd%25d1%258c-%25d0%25b8%25d0%25b7-%25d0%25b6%25d0%25b8%25d0%25b7%25d0%25bd%25d0%25b8-%25d1%2587%25d0%25b5%25d0%25bb%25d0%25be%25d0%25b2%25d0%25b5%25d0%25ba%25d0%25b0-%25d0%25be%25d0%25b4%25d0%25bd%25d0%25b0%25d0%25b6%25d0%25b4%25d1%258b-%25d1%2580%2F">
+<link rel="alternate" type="text/xml+oembed" href="http://monoporn.ru/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fmonoporn.ru%2F2017%2F10%2F17%2F%25d0%25be%25d0%25b4%25d0%25b8%25d0%25bd-%25d0%25b4%25d0%25b5%25d0%25bd%25d1%258c-%25d0%25b8%25d0%25b7-%25d0%25b6%25d0%25b8%25d0%25b7%25d0%25bd%25d0%25b8-%25d1%2587%25d0%25b5%25d0%25bb%25d0%25be%25d0%25b2%25d0%25b5%25d0%25ba%25d0%25b0-%25d0%25be%25d0%25b4%25d0%25bd%25d0%25b0%25d0%25b6%25d0%25b4%25d1%258b-%25d1%2580%2F&amp;format=xml">
+		<style type="text/css">
+			.site-logo { margin-top: px; padding-bottom: px; }
+			.entry-content a, .entry-summary a, .comment-content a, .comment-respond a, .navigation a, .comment-navigation a, .current-menu-item > a { color:; }
+			a:hover { color:; }
+		</style>
+	</head>
+
+<body class="post-template-default single single-post postid-402 single-format-standard no-sidebar footer-0 has-avatars">
+<div id="page" class="hfeed site">
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+						<h1 class="site-title"><a href="http://monoporn.ru/" rel="home">monoporn</a></h1>
+									<div class="site-description">don't read</div>
+					</div>
+		<div class="main-navigation-wrapper">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<h1 class="menu-toggle">Меню</h1>
+				<a class="skip-link screen-reader-text" href="http://monoporn.ru/2017/10/17/%d0%be%d0%b4%d0%b8%d0%bd-%d0%b4%d0%b5%d0%bd%d1%8c-%d0%b8%d0%b7-%d0%b6%d0%b8%d0%b7%d0%bd%d0%b8-%d1%87%d0%b5%d0%bb%d0%be%d0%b2%d0%b5%d0%ba%d0%b0-%d0%be%d0%b4%d0%bd%d0%b0%d0%b6%d0%b4%d1%8b-%d1%80/#content">Перейти к содержимому</a>
+				<div class="menu-%d0%bc%d0%b5%d0%bd%d1%8e-1-container"><ul id="menu-%d0%bc%d0%b5%d0%bd%d1%8e-1" class="menu"><li id="menu-item-149" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-149"><a href="http://monoporn.ru/category/news/">Новости</a></li>
+<li id="menu-item-139" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-139"><a href="http://monoporn.ru/category/notes_cat/">Заметки</a></li>
+<li id="menu-item-140" class="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-140"><a href="http://monoporn.ru/category/words_cat/">Рассказы</a></li>
+<li id="menu-item-276" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-276"><a href="http://monoporn.ru/%d0%ba%d0%bd%d0%b8%d0%b3%d0%b0/">Книги</a></li>
+<li id="menu-item-138" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-138"><a href="http://monoporn.ru/contact/">Контакты</a></li>
+</ul></div>											</nav><!-- #site-navigation -->
+		</div>
+			</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		
+			
+<article id="post-402" class="post-402 post type-post status-publish format-standard hentry category-words_cat">
+	<header class="entry-header">
+		<h1 class="entry-title">«Один день из жизни человека, однажды разочаровавшегося в сыре».</h1>
+			<div class="entry-meta">
+		<span class="posted-on">
+			<time class="entry-date published" datetime="2017-10-17T23:17:47+00:00">Октябрь 17, 2017</time>		</span>
+					</div><!-- .entry-meta -->
+				</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<p>Мы стояли с ней на вокзале, прощались. У ней слезы на глазах, а я думал, почему сыр в магазинах такой странный на вкус стал. Ужас просто, что за сыр.<br>
+— Ты никого больше так любить не сможешь, — говорит она.<br>
+— Ага, — отвечаю.<br>
+— Вспоминать каждую ночь обо мне будешь.<br>
+— Если не засну – то само собой.<br>
+— Не заснешь ведь.<br>
+Она мне письмо в руку сунула, потом уже садиться было нужно. Денег еще мне предложила, но я отказался. Нехорошо это – деньги брать, учитывая, что я и так вчера у нее пару сотен вытащил, пока она чемодан собирала..<br>
+<span id="more-402"></span>Подождал, пока поезд уже уедет, потом поехал к другой своей знакомой. Вегетарианке – если это кому-то важно.<br>
+— ЧТО ЗА ПАРШИВАЯ ТВАРЬ! – сказала она, услышав от меня эту историю.<br>
+— Подожди, милая, я не рассказал еще самое главное.<br>
+— БЕДНАЯ, БЕДНАЯ ДЕВУШКА! КАК ТЫ МОГ!<br>
+Тем не менее, вино она нам разлила. Подогрела даже, надеясь на лучшее, достала сигареты, сидит, курит, как будто и нет в этом ничего вредного. Недостаток мяса восполняет.<br>
+— Не надо шутить на эту тему.<br>
+— Тогда слушай, что расскажет тебе мой сахарный язык.<br>
+Все началось около полугода назад. Я был голоден и безумен, но чувствительность не потерял. Обострил – наоборот. Майский ветер вливался в окна, а мне по почте письмо пришло. Я обычно такие письма – которые по почте приходят – выбрасываю сразу, а это открыл зачем-то.<br>
+— Господи, и что там?<br>
+— Да счета, как обычно.<br>
+— Глупое начало.<br>
+Знаю. Но потом она появилась. В платье – а ты знаешь, что я думаю насчет платьев, и тонкая шея у нее, и глаза — добрые. Те, у кого глаза добрые – в постели лучше, но лучше – с теми, у кого вообще постель есть.<br>
+— А у нее есть?<br>
+— Мы как раз смотреть отправились.<br>
+Приходим – и хорошо все. Хомяк в клетке спал, она свои любимые книги показала. Я таких и не знаю – кергигор какой-то, фуго – извращенцы, наверное. Я сижу, смотрю в ее огромные добрые глаза, представляю, как хорошенько ее сейчас отхлещу.<br>
+А она внезапно нож к горлу мне приставляет и говорит:<br>
+— Ты, наверное, жить со мной хочешь?<br>
+Я сказать ничего не могу, киваю лишь, что мол – просто жить хочу. Она еще сильнее его прижимает:<br>
+— ТОЧНО ХОЧЕШЬ, УБЛЮДОК?<br>
+Я киваю из-за всех сил, про себя думаю – что со мной не так, почему опять вокруг сумасшедшие. Но она нож отодвинула, и хохотать начала. «До чего смешно с тобой!» — говорит. А сама при этом раздевается, прямо пуговицы от блузки отлетают, и ко мне лезет. Я и бежать уже хочу, а губы ее впиваются, и язык такой мокрый, свежий, и грудь левая вываливается, и она ею об мою щеку трется, а хомяк в клетке носится как безумный, и пищит, пищит..<br>
+В общем, пару дней все хорошо было. Я даже подумал, что оно и к лучшему. Поладим – тут главное слабину не дать, про звезды не заговорить. Я и не говорил – пока однажды ночью просыпаюсь, а она – окна досками заколачивает. Гвозди здоровенные вбивает, со всей силы.<br>
+— Милая, что это ты делаешь?<br>
+— Это чтобы тебя через окно не утащили, — отвечает.<br>
+Смотрю – ноги мои связаны, руки скотчем обмотаны, ни пошевелиться, ни убежать.<br>
+— Кто же меня утащит через окно? – спрашиваю.<br>
+— Крабы.<br>
+Вино оказалось крепленым. Я задел свой стакан, и на стол все полилось. Надо было чем-то промокнуть, я письмо в кармане нащупал – тоже подойдет, и стал им тереть. Вроде впиталось.<br>
+— Ты наверно решил, что это очаровательно – крабы, — раскуривала она вторую.<br>
+— Я в отчаянии был, а она — утром тебя развяжу, — говорит. Улыбается при этом – мило-мило, как продавцы в супермаркетах. Разделась и полезла снова, с этим мокрым и свежим языком, с этой грудью..<br>
+Потом она говорит, что таблетки пьет. Только они не помогают ни черта. Только хуже от них. Что за таблетки? – спрашиваю, — знаешь, ненароком, чтобы не обидеть, как бы между делом спросил. А она прямо банку мне с ними достала – на, смотри.<br>
+— Так сразу тебе их и показала?<br>
+— Ну.<br>
+Я подумал, что буду сам их ей подмешивать, хочет она того или нет. Аккуратно, в еду добавлять. Целый месяц выгадывал, пока она отворачивалась, всякие способы выдумывал – но жрала она их – это точно. И знаешь – спокойная была, я уже почти удрать решился.<br>
+— То есть помогали таблетки? – подлила она нам вино.<br>
+— Ты дальше слушай.<br>
+В конце месяца сидим с ней, ужинаем, все хорошо. Хочу сказать ей, что ухожу, с мыслями собираюсь, а она встает – прямо на стол, платье с себя срывает и орет дурным голосом, заливается.<br>
+— Таблетки – аскорбинки были, УРОД! — и смеется при этом, смеется.<br>
+Я пробую – ну точно, аскорбинки.<br>
+Потом все совсем жутко стало. У нее приступы начались, она и на стены набрасывалась, и на меня. Однажды чуть не съела. Снова нож приставила, только на этот раз это тесак здоровенный был – ГОВОРИ ГОД НЕЗАВИСИМОСТИ УГАНДЫ – шестьдесят второй? – наугад отвечаю я.<br>
+— НЕПРАВИЛЬНО! — и ножом по пальцу. Потом ее отпускает, она плачет и бинтами пальцы перевязывает. Спирт на них льет. Боль адская.<br>
+— Слушай, но год независимости Уганды вроде и правда шестьдесят второй?<br>
+— Да знаю я.<br>
+А недавно совсем – когда я о самоубийстве уже начал думать, она говорит, что устала и нам надо расстаться.<br>
+— Не может быть.<br>
+— Сам сначала не поверил, думаю – опять уловка. А она нет – чемодан собирает, все дела. Я у ней несколько сотен выкрал – своих денег у меня уже давно не было. Попросила только до вокзала ее проводить.<br>
+— Странное какое-то вино, не находишь?<br>
+— Да меня самого разводит жутко.<br>
+— Не помню, откуда оно у меня – подарили что ли?<br>
+Она пошла мусор выкидывать, а я лег на диван, и тут же заснул. Спокойным сном за долгие месяцы. Я спал, и мне снился фильм Вуди Аллена, тот, в котором по цветочным лугам гигантская грудь ползает, а все её поймать пытаются. «Вуди, — говорю я ему, — ну сдалась тебе эта гигантская грудь, давай лучше нормальные фильмы еще поснимаем, пока ты не умер». «ТЫ НИЧЕГО НЕ ПОНИМАЕШЬ В НЕЗАВИСИМОМ КИНО!», — кричит мне он, а после к нему подходят красивые интеллектуальные девчушки и уводят куда-то. Я один опять остаюсь. Стою, смотрю на эту гигантскую грудь и думаю – как бы и мне тоже начать понимать в независимом кино..<br>
+Проснулся, моей знакомой не было. За овощами наверное пошла, или куда ходят эти ребята.<br>
+Потянулся, нащупал пол. Пошел на кухню, чтобы налить воды – спросонья, еще полуслепой. Беру стакан, поднимаю взгляд – и стакан медленно падает на пол.<br>
+Около раковины стоит ОНА и нож отмывает. Нож в красном чем-то весь, в буром практически. На столе – письмо ее лежит, расправленное, почищенное.<br>
+— Я знала, что ты не сможешь меня забыть, — говорит.<br>
+И улыбается, улыбается.<br>
+Улыбается.</p>
+<p>p.s. практически оммаж понятно кому.</p>
+			</div><!-- .entry-content -->
+		<footer class="entry-meta entry-footer">
+				
+
+					</footer><!-- .entry-meta -->
+	</article><!-- #post-## -->
+
+				<nav class="navigation post-navigation" role="navigation">
+		<div class="nav-links">
+
+			<div class="nav-previous"><h2>Предыдущая запись</h2><a href="http://monoporn.ru/2017/09/21/%d0%bf%d0%be-%d0%bc%d0%bd%d0%b5-%d1%82%d0%b0%d0%ba-%d0%b2%d1%81%d0%b5%d0%b3%d0%be-%d0%bb%d0%b8%d1%88%d1%8c-%d1%80%d0%b0%d1%81%d1%81%d0%ba%d0%b0%d0%b7-%d0%bd%d0%be-%d0%b4%d0%bb%d1%8f-%d1%82%d0%b5/" rel="prev">«Маковые поля наших возможностей»</a></div>			<div class="nav-next"><h2>Следующая запись</h2><a href="http://monoporn.ru/2017/10/25/%d0%b8%d0%b7-%d0%bf%d0%be%d1%82%d0%b5%d1%80%d1%8f%d0%bd%d0%bd%d0%be%d0%b3%d0%be/" rel="next">«из потерянного..»</a></div>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	
+			
+		
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+			(2013-2018) 18+<br>
+			<div class="site-copyright"><a href="http://monoporn.ru/" rel="home">monoporn</a></div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<script type="text/javascript" src="./index_files/jquery.colorbox.js.Без названия"></script>
+<script type="text/javascript" src="./index_files/jquery.colorbox-ru.js.Без названия"></script>
+<script type="text/javascript" src="./index_files/navigation.js.Без названия"></script>
+<script type="text/javascript" src="./index_files/skip-link-focus-fix.js.Без названия"></script>
+<script type="text/javascript" src="./index_files/wp-embed.min.js.Без названия"></script>
+
+
+<div id="cboxOverlay" style="display: none;"></div><div id="colorbox" class="" role="dialog" tabindex="-1" style="display: none;"><div id="cboxWrapper"><div><div id="cboxTopLeft" style="float: left;"></div><div id="cboxTopCenter" style="float: left;"></div><div id="cboxTopRight" style="float: left;"></div></div><div style="clear: left;"><div id="cboxMiddleLeft" style="float: left;"></div><div id="cboxContent" style="float: left;"><div id="cboxTitle" style="float: left;"></div><div id="cboxCurrent" style="float: left;"></div><button type="button" id="cboxPrevious"></button><button type="button" id="cboxNext"></button><button id="cboxSlideshow"></button><div id="cboxLoadingOverlay" style="float: left;"></div><div id="cboxLoadingGraphic" style="float: left;"></div></div><div id="cboxMiddleRight" style="float: left;"></div></div><div style="clear: left;"><div id="cboxBottomLeft" style="float: left;"></div><div id="cboxBottomCenter" style="float: left;"></div><div id="cboxBottomRight" style="float: left;"></div></div></div><div style="position: absolute; width: 9999px; visibility: hidden; display: none; max-width: none;"></div></div><!----></body></html>
